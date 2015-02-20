5ef44fc36db431c3e97a6f3364934fc17a9b6a27
DreamingFighter <DreamingFighter@gmail.com>
2015-02-20 12:20:18 +0200

    Work for 20.02.2015

--- big_panel.php
+++ big_panel.php
@@ -8,9 +8,14 @@
     <style>
 		.gmap3{
 			margin: 20px auto;
-			border: 1px dashed #C0C0C0;
+			
 			width: 500px;
 			height: 250px;
+		}
+		.button
+		{
+			width:100px;
+			float:left;
 		}
 		
     </style>
@@ -22,25 +27,92 @@
 	<script type="text/javascript" src="javascript/gmap3/gmap3.js"></script>
 	
 	<script type="text/javascript">
-	var chicago = new google.maps.LatLng(65.013130, 25.476192);
-	function HomeControl(map) {
+	var showMenu = null, fullMenu = null;
+	function ShowMenuButton(map) 
+	{
 		var $container = $(document.createElement('DIV')),
 			$outer = $(document.createElement('DIV')),
 			$inner = $(document.createElement('DIV'));
         
-		$inner.addClass("inner").html("<img style='margin-bottom:50px;' src='images/open_button.png'/>");
-		$outer.addClass("outer").attr('title', "Click to set the map to Home");
-		$container.attr("id", "homeControl");
+		$inner.addClass("inner").html("<div style='margin-bottom:50px;border:2px solid;border-color: #2a3333;border-radius: 6px;background-color: white;width:73px;height:68px;'><img style='' src='images/open_button.png' alt='Smiley face'/></div>");
+		$container.addClass("outer").attr('title', "Click to set the map to Home");
+		$container.attr("id", "showMenu");
       
 		$container.append( $outer.append( $inner ) );
       
 		google.maps.event.addDomListener($outer.get(0), 'click', function() {
-			map.setCenter(chicago)
+			$('#showMenu').fadeOut();
+			$('#fullMenu').fadeIn();
 		});
       
 		this.index = 1;
 		map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push($container.get(0));
     }
+	
+	function Menu(map)
+	{
+		var $container = $(document.createElement('DIV'));
+		$container.attr("id", "fullMenu");
+		$container.attr("style", "display:none;margin-left:-77px;margin-bottom:50px;border:2px solid;border-color: #2a3333;border-radius: 6px;background-color: white;width:800px;height:68px;");
+		
+		
+		var $closeButton = $(document.createElement('DIV'));
+		$closeButton.attr("class", "button");
+		$closeButton.attr("style", "margin-left:3px");
+		$closeButton.html("<img src='images/close_button.png' />");
+		google.maps.event.addDomListener($closeButton.get(0), 'click', function() {
+			$('#fullMenu').fadeOut();
+			$('#showMenu').fadeIn();
+		});
+		
+		var $navigationButton = $(document.createElement('DIV'));
+		$navigationButton.attr("class", "button");
+		$navigationButton.html("<center><img src='images/navigation_button.png' /><div>Navigation</div></center>");
+		google.maps.event.addDomListener($navigationButton.get(0), 'click', function() {
+			$('#navigationForm').fadeIn();
+		});
+		
+		var $busTimetableButton = $(document.createElement('DIV'));
+		$busTimetableButton.attr("class", "button");
+		$busTimetableButton.html("<center><img src='images/bus_timetable_button.png' /><div>Timetable</div></center>");
+		google.maps.event.addDomListener($busTimetableButton.get(0), 'click', function() {
+			//TODO
+		});
+		
+		var $placeButton = $(document.createElement('DIV'));
+		$placeButton.attr("class", "button");
+		$placeButton.html("<center><img src='images/place_button.png' /><div>Place</div></center>");
+		google.maps.event.addDomListener($placeButton.get(0), 'click', function() {
+			//TODO
+		});
+		
+		var $trafficCongestionButton = $(document.createElement('DIV'));
+		$trafficCongestionButton.attr("class", "button");
+		$trafficCongestionButton.html("<center><img src='images/traffic_congestion_button.png' /><div>Traffic Congestion</div></center>");
+		google.maps.event.addDomListener($trafficCongestionButton.get(0), 'click', function() {
+			//TODO
+		});
+		
+		var $resetCSS = $(document.createElement('DIV'));
+		$resetCSS.attr("style", "clear:both");
+		
+		$container.append($closeButton);
+		$container.append($navigationButton);
+		$container.append($busTimetableButton);
+		$container.append($placeButton);
+		$container.append($trafficCongestionButton);
+		
+		$container.append($resetCSS);
+		map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push($container.get(0));
+	}
+	
+	function Navigation(map)
+	{
+		var $container = $(document.createElement('DIV'));
+		$container.attr("id", "navigationForm");
+		$container.attr("style", "display:none;margin-left:-77px;margin-bottom:150px;border:2px solid;border-color: #2a3333;border-radius: 6px;background-color: white;width:800px;height:200px;");
+		map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push($container.get(0));
+	}
 	
     $(function(){
         $("#test").gmap3({
@@ -63,7 +135,9 @@
 				},
 				
 				callback: function(map){
-					new HomeControl(map);
+					showMenu = new ShowMenuButton(map);
+					fullMenu = new Menu(map);
+					new Navigation(map);
 				}
 			}
         });
@@ -74,7 +148,7 @@
   </head>
 	
     
-	<div style="border:10px solid;border-color: #2a3333;border-radius: 25px;width:100px;width:960px;height:486px" id="test"></div>
+	<div style="border:10px solid;border-color: #2a3333;border-radius: 25px;width:960px;height:486px" id="test"></div>
   <body>
 
   </body>
