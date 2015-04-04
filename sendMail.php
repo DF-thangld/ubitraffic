84fb583234a8b9aa60a13d3cb41e3b5c94f1e328
Tommi <tommi.puolamaa@student.oulu.fi>
2015-04-04 21:37:36 +0300

    Added PHPMailer library and sendMail.php

---
+++ sendMail.php
@@ -0,0 +1,38 @@
+<?php
+require '/libraries/PHPMailer/PHPMailerAutoload.php';
+
+$destination = $_POST['dest'];
+$email = new PHPMailer;
+
+//set SMTP
+$email->isSMTP();                                      // Set mailer to use SMTP
+$email->Host = 'smtp.gmail.com';  					   // Specify main SMTP server
+$email->SMTPAuth = true;                               // Enable SMTP authentication
+$email->Username = 'ubitraffic2015@gmail.com';         // SMTP username
+$email->Password = 'dlrigjy5867';                      // SMTP password
+$email->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$email->Port = 587;    
+
+//select from and to
+$email->From      = 'ubitraffic2015@gmail.com';
+$email->FromName  = 'UbiTraffic';
+$email->addAddress($destination);
+
+//add attachment
+$email->addAttachment('images/png/testing.png', 'UbiTraffic_map.png');
+$email->isHTML(true);  
+
+
+//set subject and body of the e-mail
+$email->Subject   = 'UbiTraffic downloaded map '.date('d.m.Y');
+$email->Body      = 'Hi!<br /><br />Please find your requested map image attached to this e-mail.<br /><br />Best Regards:<br /> UbiTraffic Team';
+$email->AltBody = 'Hi! Please find your requested map image attached to this e-mail. Best Regards: UbiTraffic Team';
+
+
+if(!$email->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $email->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
