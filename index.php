08d8ed2d1c2548c07728ab720cc5c832f56b8082
DreamingFighter <DreamingFighter@gmail.com>
2015-05-19 12:02:26 +0300

    Oulu places

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header("Location: http://vm.node0002.ubioulu.fi:8080/tandembrowsing/?session=node0002"); /* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
